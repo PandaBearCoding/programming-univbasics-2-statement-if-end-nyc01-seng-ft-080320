a8362cc80d34c58ad8d0a417aad6d5d8b56ed031
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-27 16:16:26 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -2,14 +2,8 @@
 
 current_time = Time.now
 current_time = current_time.to_i
-if current_time % 2 == 0
-  puts "Even!"
-else current_time == odd
-  puts "Odd!"
-end 
 
-
-if Time.now.to_i % 2 == 0
+if current_time.even?
   puts "Even!"
 else
   puts "Odd!"
